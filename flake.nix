6b40809b7c1317808cf17d387b542bd066103828
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-15 09:23:20 +0200

    feat: packaged rendercanvas

--- flake.nix
+++ flake.nix
@@ -12,6 +12,21 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        rendercanvas = pkgs.python3Packages.buildPythonPackage {
+          pname = "rendercanvas";
+          version = "v2.2.1";
+          format = "pyproject";
+          nativeBuildInputs = with pkgs.python3Packages; [
+            flit-core
+            sniffio
+          ];
+          src = pkgs.fetchFromGitHub {
+            owner = "pygfx";
+            repo = "rendercanvas";
+            rev = "v2.2.1";
+            sha256 = "sha256-Wtil5SKMnL3SkbqWHU3hHKz73D+ZQtQUG06mSb6JE08=";
+          };
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -19,6 +34,7 @@
             poetry
             python3
             python313Packages.wgpu-py
+            rendercanvas
           ];
         };
       }
